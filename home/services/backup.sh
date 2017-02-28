07b6ce63bab6c12ba6d7fa6cff4f11cd6bb1422a
James L Vodanovich <james@jvodan.com>
2017-02-28 11:44:17 +1100

    update

---
+++ home/services/backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#custom backup script this namespaced service 
+#not here if nothing in it
---
+++ home/services/repace.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#custom nackup script this namespaced service 
+#not here if nothing in it
